f9fb85c04db62ec001e0acf79bfca139ca02994b
em-3 <69275008+em-3@users.noreply.github.com>
2024-06-12 14:07:12 -0400

    Fixed main plugin class using private constructor

--- src/main/kotlin/dev/em_3/simplespectatormode/SimpleSpectatorMode.kt
+++ src/main/kotlin/dev/em_3/simplespectatormode/SimpleSpectatorMode.kt
@@ -4,12 +4,13 @@ import org.bukkit.NamespacedKey
 import org.bukkit.event.Listener
 import org.bukkit.plugin.java.JavaPlugin
 
-object SimpleSpectatorMode : JavaPlugin(), Listener {
-
-    lateinit var COORDINATES_KEY: NamespacedKey
+class SimpleSpectatorMode : JavaPlugin(), Listener {
 
     override fun onEnable() {
-        COORDINATES_KEY = NamespacedKey(SimpleSpectatorMode, "safe-coordinates")
+        COORDINATES_KEY = NamespacedKey(this, "safe-coordinates")
     }
 
+    companion object {
+        lateinit var COORDINATES_KEY: NamespacedKey
+    }
 }
